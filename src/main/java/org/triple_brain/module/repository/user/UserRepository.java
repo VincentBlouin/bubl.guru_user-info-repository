a734521900a6870beae6b1f8bfd39e5a8a8cbfdb
Vincent Blouin <vincent.blouin@gmail.com>
2015-03-09 07:28:34 -0400

    can_find_user_by_email

--- src/main/java/org/triple_brain/module/repository/user/UserRepository.java
+++ src/main/java/org/triple_brain/module/repository/user/UserRepository.java
@@ -8,7 +8,7 @@ import org.triple_brain.module.model.User;
 import org.codehaus.jettison.json.JSONObject;
 
 public interface UserRepository {
-    void save(User user);
+    void createUser(User user);
     User findById(String id) throws NonExistingUserException;
     User findByUsername(String username) throws NonExistingUserException;
     User findByEmail(String email) throws NonExistingUserException;
