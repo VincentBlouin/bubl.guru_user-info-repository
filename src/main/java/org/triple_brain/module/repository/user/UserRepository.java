e3b65b834974d4377c34e0f4e042492cf8943ffd
Vincent Blouin <vincent.blouin@gmail.com>
2015-03-13 08:32:17 -0400

    user repository

--- src/main/java/org/triple_brain/module/repository/user/UserRepository.java
+++ src/main/java/org/triple_brain/module/repository/user/UserRepository.java
@@ -3,13 +3,11 @@
  */
 
 package org.triple_brain.module.repository.user;
-import org.codehaus.jettison.json.JSONException;
+
 import org.triple_brain.module.model.User;
-import org.codehaus.jettison.json.JSONObject;
 
 public interface UserRepository {
     void createUser(User user);
-    User findById(String id) throws NonExistingUserException;
     User findByUsername(String username) throws NonExistingUserException;
     User findByEmail(String email) throws NonExistingUserException;
     Boolean usernameExists(String username);
