51b909eed77aae524c7ce4c53f24750b5a8049fe
Vincent Blouin <vincent.blouin@gmail.com>
2015-04-01 17:13:24 -0400

    User creation progress

--- src/main/java/org/triple_brain/module/repository/user/UserRepository.java
+++ src/main/java/org/triple_brain/module/repository/user/UserRepository.java
@@ -7,7 +7,7 @@ package org.triple_brain.module.repository.user;
 import org.triple_brain.module.model.User;
 
 public interface UserRepository {
-    void createUser(User user);
+    User createUser(User user);
     User findByUsername(String username) throws NonExistingUserException;
     User findByEmail(String email) throws NonExistingUserException;
     Boolean usernameExists(String username);
